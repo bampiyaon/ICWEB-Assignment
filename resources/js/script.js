737560990bc63b5c39e11ef8924264a27f2dddfa
bampiyaon <piyaon-p@hotmail.com>
2021-06-16 18:09:36 +0700

    [EDIT] all list except section 2, menu, mobile

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,26 +1,24 @@
-const counters = document.querySelectorAll('.counter');
-const speed = 1000; // The lower the slower
 
-counters.forEach(counter => {
-	const updateCount = () => {
-		const target = +counter.getAttribute('data-target');
-		const count = +counter.innerText;
+    const counters = document.querySelectorAll('.counter');
+    const speed = 1000;
+    
+    counters.forEach(counter => {
+        const updateCount = () => {
+            const target = +counter.getAttribute('data-target');
+            const count = +counter.innerText;
+    
+            const inc = target / speed;
+    
+            if (count < target) {
+                counter.innerText = Math.ceil(count + inc);
+                setTimeout(updateCount, 1);
+            } else {
+                counter.innerText = target;
+            }
+        };
+    
+        updateCount();
+    });
 
-		// Lower inc to slow and higher to slow
-		const inc = target / speed;
-
-		// Check if target is reached
-		if (count < target) {
-			// Add inc to count and output in counter
-			counter.innerText = Math.ceil(count + inc);
-			// Call function every ms
-			setTimeout(updateCount, 1);
-		} else {
-			counter.innerText = target;
-		}
-	};
-
-	updateCount();
-});
 
   
